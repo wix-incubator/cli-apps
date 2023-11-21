ee34f5624fad51b93beb0076532b89b9efbe8229
Netanel Gilad <netanelg@wix.com>
2023-11-21 11:08:00 +0200

    not needed

--- packages/wix-graphql-explorer/vitest.config.ts
+++ packages/wix-graphql-explorer/vitest.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vitest/config';
-import isCI from 'is-ci';
 
 export default defineConfig({
 	test: {
 		setupFiles: ['./test/setup.ts'],
 		environment: 'jsdom',
-		...(isCI && {
-			reporters: ['vitest-teamcity-reporter'],
-		}),
 	},
 });
