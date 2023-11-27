e146c05faa95122d2e83a33d59fb799de816856d
Ronny Ringel <ronnyr@wix.com>
2023-11-27 13:36:18 +0200

    [contentful] refactor props #pr (#17)

--- packages/contentful/src/dashboard/components/Page/Page.tsx
+++ packages/contentful/src/dashboard/components/Page/Page.tsx
@@ -5,12 +5,14 @@ import PageLayout from '../PageLayout/PageLayout';
 import React, { useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { getAppInstance, getInstanceId } from '../../utils';
-import { CONNECT_URL } from '../../constants/constants';
+import { CONNECT_URL, SUPPORT_CMS_URL } from '../../constants/constants';
+import { useListingExternalDataServices } from '../SettingsFormLayout/use-external-data-services';
+import { LoadingLayout } from '../LoadingLayout/LoadingLayout';
 
 export const Page = () => {
 	const {t} = useTranslation();
 	const [upgradeButton, setUpgradeButton] = useState({show: false, endOfTrialDate: ''});
-	const [showMissingConnection, setShowMissingConnection] = useState(false);
+	const { data, isLoading } = useListingExternalDataServices();
 
 	useEffect(() => {
 		const instanceId = getInstanceId();
@@ -22,13 +24,13 @@ export const Page = () => {
 	}, []);
 
 	const Link = ({children}: {children?: React.ReactNode}) => {
-		return <a href="https://support.wix.com/en/article/cms-formerly-content-manager-getting-started" target="_blank" rel="noreferrer">{children}</a>;
+		return <a href={SUPPORT_CMS_URL} target="_blank" rel="noreferrer">{children}</a>;
 	};
 
-	return (
+	return isLoading ? <LoadingLayout /> : (
 		<PageLayout title={t('app.title')!} dataHook="app-title" showUpgradeButton={upgradeButton.show} endOfTrialDate={upgradeButton.endOfTrialDate}>
 			<Cell>
-				{showMissingConnection ?
+				{data && !data!.length ?
 					<SectionHelper dataHook="missing-connection" appearance="danger" title={t('contentful.settings.form.missingConfiguration.title')} actionText={t('contentful.settings.form.missingConfiguration.action')!} onAction={() => {
 						window.open(`${CONNECT_URL}${getAppInstance()}`, '_blank');
 					}}>
@@ -36,7 +38,7 @@ export const Page = () => {
 					</SectionHelper> : <CardLayout
 						title={t('contentful.settings.card.title')!}
 					>
-						<SettingsForm setShowMissingConnection={setShowMissingConnection}/>
+						<SettingsForm externalListData={data!}/>
 					</CardLayout>}
 			</Cell>
 		</PageLayout>
--- packages/contentful/src/dashboard/constants/constants.ts
+++ packages/contentful/src/dashboard/constants/constants.ts
@@ -3,3 +3,4 @@ export const APP_ID = '9f6d5767-4aea-4de0-93bc-ae381c513365';
 export const UPGRADE_URL = `https://www.wix.com/apps/upgrade/${APP_ID}?appInstanceId=`;
 export const DOCS_URL = 'https://github.com/wix-incubator/cli-apps/tree/master/packages/contentful';
 export const CONNECT_URL = 'https://ronnyr34.wixsite.com/oauth-contentful/_functions/redirectToContentfulFromBM?authorization=';
+export const SUPPORT_CMS_URL = 'https://support.wix.com/en/article/cms-formerly-content-manager-getting-started';
