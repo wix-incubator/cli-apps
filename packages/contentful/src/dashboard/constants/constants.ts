62e5d70bfe49c8b9ddb793c55f09e781f4927f45
ronnyr <ronnyr@wix.com>
2023-11-27 14:09:40 +0200

    [contentful] refactor upgrade section #pr

--- packages/contentful/src/dashboard/constants/constants.ts
+++ packages/contentful/src/dashboard/constants/constants.ts
@@ -1,6 +1,8 @@
 export const HOST = 'api.contentful.com';
+const DOMAIN_SERVERLESS = 'https://ronnyr34.wixsite.com/oauth-contentful/_functions';
 export const APP_ID = '9f6d5767-4aea-4de0-93bc-ae381c513365';
 export const UPGRADE_URL = `https://www.wix.com/apps/upgrade/${APP_ID}?appInstanceId=`;
 export const DOCS_URL = 'https://github.com/wix-incubator/cli-apps/tree/master/packages/contentful';
-export const CONNECT_URL = 'https://ronnyr34.wixsite.com/oauth-contentful/_functions/redirectToContentfulFromBM?authorization=';
+export const CONNECT_URL = `${DOMAIN_SERVERLESS}/redirectToContentfulFromBM?authorization=`;
+export const CHECK_PREMIUM_URL = `${DOMAIN_SERVERLESS}/connectionPermissions?instanceId=`;
 export const SUPPORT_CMS_URL = 'https://support.wix.com/en/article/cms-formerly-content-manager-getting-started';
