e3ddcf6d157ad7ec1be0c2af0fdb717ae6c059aa
carmelc <carmelc@wix.com>
2024-01-01 13:01:17 +0200

    support app on contentful market mode

--- packages/contentful/src/dashboard/components/Page/Page.tsx
+++ packages/contentful/src/dashboard/components/Page/Page.tsx
@@ -4,10 +4,9 @@ import { SettingsForm } from '../SettingsFormLayout/SettingsForm/SettingsForm';
 import PageLayout from '../PageLayout/PageLayout';
 import React, { useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { getAppInstance, getInstanceId } from '../../utils';
+import { getContentfulAuthorizeUrl, getInstanceId} from '../../utils';
 import {
 	CHECK_PREMIUM_URL,
-	CONNECT_URL,
 	DEV_MODE_NOT_SET_ERROR_CODE,
 	SUPPORT_CMS_URL, SUPPORT_DEV_MODE_URL,
 	UPGRADE_URL
@@ -42,20 +41,20 @@ export const Page = () => {
 		<PageLayout title={t('app.title')!} dataHook="app-title" showUpgradeButton={upgradeButton.show}>
 			{upgradeButton.show &&
 				<Cell>
-					<SectionHelper dataHook="upgrade-section" appearance={upgradeButton.isTrialEnded ? 'danger' : 'premium'} title={t('contentful.settings.card.upgrade.title')} actionText={t('contentful.settings.card.upgrade.action')!} onAction={openPremiumPage}>
+					<SectionHelper fullWidth dataHook="upgrade-section" appearance={upgradeButton.isTrialEnded ? 'danger' : 'premium'} title={t('contentful.settings.card.upgrade.title')} actionText={t('contentful.settings.card.upgrade.action')!} onAction={openPremiumPage}>
 						{upgradeButton.isTrialEnded ? t('contentful.settings.card.trialEnded'): t('contentful.settings.card.trialEnd', {endOfTrialDate: upgradeButton.endOfTrialDate})}
 					</SectionHelper>
 				</Cell>
 			}
 			<Cell>
 				{data && !data!.length && !upgradeButton.isTrialEnded ?
-					<SectionHelper dataHook="missing-connection" appearance="danger" title={t('contentful.settings.form.missingConfiguration.title')} actionText={t('contentful.settings.form.missingConfiguration.action')!} onAction={() => {
-						window.open(`${CONNECT_URL}${getAppInstance()}`, '_blank');
+					<SectionHelper fullWidth dataHook="missing-connection" appearance="danger" title={t('contentful.settings.form.missingConfiguration.title')} actionText={t('contentful.settings.form.missingConfiguration.action')!} onAction={() => {
+						window.open(getContentfulAuthorizeUrl(), '_blank');
 					}}>
 						<Trans i18nKey="contentful.settings.form.missingConfiguration.message" components={{1: (<SupportCmsLink/>)}}/>
 					</SectionHelper> :
 					isError ?
-						<SectionHelper dataHook="connection-error-helper" appearance="danger" title={t('contentful.settings.form.errorConfiguration.title')}>
+						<SectionHelper fullWidth dataHook="connection-error-helper" appearance="danger" title={t('contentful.settings.form.errorConfiguration.title')}>
 							<Trans
 								i18nKey={(error as any)?.details?.applicationError?.code === DEV_MODE_NOT_SET_ERROR_CODE ?
 									'contentful.settings.form.errorConfiguration.noDevMode.message' :
---
+++ packages/contentful/src/dashboard/services/access-token-for-connection.ts
@@ -0,0 +1,18 @@
+import {ExternalDatabaseConnection} from "@wix/data/build/cjs/src/data-v1-external-database-connection.universal";
+
+
+export default async (configuration: ExternalDatabaseConnection['configuration'], instance: string) => {
+  return fetch('https://www.wixapis.com/contentful-funnel-nextjs/connection/contentful/access', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': instance,
+    },
+    body: JSON.stringify({
+      requestContext: {
+        settings: configuration,
+      },
+    }),
+  }).then((res) => res.json());
+};
+
